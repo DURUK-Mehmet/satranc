94f2b9534f0e036666b3c79c811ba5068df2f44b
Salih <59319209+DURUK-Mehmet@users.noreply.github.com>
2022-07-20 18:26:52 +0300

    Add files via upload

--- Satranc.py.py
+++ Satranc.py.py
@@ -21,7 +21,6 @@ end_index=x*y
 deger=45
 print(Kontrol(deger))
 
-
 #taşlar
 #Kale
 SKale={"x1":1,
@@ -51,8 +50,6 @@ while True:#Skale57
         break
     print(SKale["x57"],SKale["y57"])
 
-#burdaki işlemleri mantığı ve sonuçlar doğrulandıktan sonra method haline getireceğim, bu şekilde uzun kodlar olmayacak 
-
 while True:#Skale64
     deger=int(input("Siyah Kale64'i nereye ilerletmek istiyorsunuz="))
     kordinatlar=Kontrol(deger)
@@ -71,63 +68,92 @@ while True:#Skale64
         break
     print(SKale["x64"],SKale["y64"])
 
-#burdaki işlemleri mantığı ve sonuçlar doğrulandıktan sonra method haline getireceğim, bu şekilde uzun kodlar olmayacak 
+while True:#Skale8
+    deger=int(input("Siyah Kale8'i nereye ilerletmek istiyorsunuz="))
+    kordinatlar=Kontrol(deger)
+    print(kordinatlar)
+    if kordinatlar[0]!=SKale["x8"] and kordinatlar[1]==SKale["y8"]:
+        SKale["x8"]=kordinatlar[0]
+        break
+    elif kordinatlar[0]==SKale["x8"] and kordinatlar[1]!=SKale["y8"]:
+        SKale["y8"]=kordinatlar[1]
+        break
+    elif kordinatlar[0]!=SKale["x8"] and kordinatlar[1]!=SKale["y8"]:
+        print("Kale sadece bir eksende ilerleyebilir")
+        break
+    else:
+        print("Kale yerinde durmuştur")
+        break
+    print(SKale["x8"],SKale["y8"])
+    
+while True:#Skale1
+    deger=int(input("Siyah Kale1'i nereye ilerletmek istiyorsunuz="))
+    kordinatlar=Kontrol(deger)
+    print(kordinatlar)
+    if kordinatlar[0]!=SKale["x1"] and kordinatlar[1]==SKale["y1"]:
+        SKale["x1"]=kordinatlar[0]
+        break
+    elif kordinatlar[0]==SKale["x1"] and kordinatlar[1]!=SKale["y1"]:
+        SKale["y1"]=kordinatlar[1]
+        break
+    elif kordinatlar[0]!=SKale["x1"] and kordinatlar[1]!=SKale["y1"]:
+        print("Kale sadece bir eksende ilerleyebilir")
+        break
+    else:
+        print("Kale yerinde durmuştur")
+        break
+    print(SKale["x1"],SKale["y1"])
+    
 
-#deger=input("Beyaz Kale1'i nereye ilerletmek istiyorsunuz=")
-#deger=input("Beyaz Kale2'i nereye ilerletmek istiyorsunuz=")
 #At
-SAt={"x1":2,
-     "y1":8,
-     "x2":7,
-     "y2":8
-     }
+At={"x2":2,
+    "y2":1,
+    "x7":7,
+    "y7":1,
+    "x58":2,
+    "y58":8,
+    "x63":7,
+    "y63":8
+    }
 #deger=input("Siyah At1'i nereye ilerletmek istiyorsunuz=")
 #deger=input("Siyah At2'i nereye ilerletmek istiyorsunuz=")
 
-BAt={"x1":2,
-     "y1":1,
-     "x2":7,
-     "y2":1
-     }
 #deger=input("Beyaz At1'i nereye ilerletmek istiyorsunuz=")
 #deger=input("Beyaz At2'i nereye ilerletmek istiyorsunuz=")
 
 #Fil
-SFil={"x1":3,
-      "y1":8,
-      "x2":6,
-      "y2":8
-      }
+Fil={"x3":3,
+     "y3":1,
+     "x6":6,
+     "y6":1,
+     "x59":3,
+     "y59":8,
+     "x62":6,
+     "y62":8
+     }
 #deger=input("Siyah Fil1'i nereye ilerletmek istiyorsunuz=")
 #deger=input("Siyah Fil2'i nereye ilerletmek istiyorsunuz=")
 
-BFil={"x1":3,
-      "y1":1,
-      "x2":6,
-      "y2":1
-      }
 #deger=input("Beyaz Fil1'i nereye ilerletmek istiyorsunuz=")
 #deger=input("Beyaz Fil2'i nereye ilerletmek istiyorsunuz=")
 
 #vezir
-SVezir={"x":4,
-        "y":8
-        }
+Vezir={"x4":4,
+       "y4":1,
+       "x60":4,
+       "y60":8
+       }
 #deger=input("Siyah Vezir'i nereye ilerletmek istiyorsunuz=")
 
-BVezir={"x":4,
-        "y":1
-        }
 #deger=input("Beayz Vezir'i nereye ilerletmek istiyorsunuz=")
 #Şah
-SSah={"x":5,
-      "y":8
-      }
+Sah={"x5":5,
+     "y5":8,
+     "x61":5,
+     "y61":8
+     }
 #deger=input("Siyah Şah'ı nereye ilerletmek istiyorsunuz=")
 
-BSah={"x":5,
-      "y":1
-      }
 #deger=input("Beyaz Şah'ı nereye ilerletmek istiyorsunuz=")
 
 
